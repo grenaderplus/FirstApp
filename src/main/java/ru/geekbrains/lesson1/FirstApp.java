f9c1acfcfe7cc42279cc893e61e7db0ce7ff47d4
grenaderplus <grenaderplus@gmail.com>
2020-08-20 12:46:26 +0300

    test

---
+++ src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Alex!");
+
+
+
+
+    }
+}
